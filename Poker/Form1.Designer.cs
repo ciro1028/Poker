09d270c198116e8a66fc26a536a52d33a63f5e06
ciro1028 <36241092+ciro1028@users.noreply.github.com>
2018-05-10 20:05:40 -0700

    commit

--- Poker/Form1.Designer.cs
+++ Poker/Form1.Designer.cs
@@ -261,11 +261,12 @@ namespace Poker
             // 
             // winnersNamesLbl
             // 
+            this.winnersNamesLbl.AutoSize = true;
             this.winnersNamesLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.winnersNamesLbl.ForeColor = System.Drawing.SystemColors.ActiveCaption;
             this.winnersNamesLbl.Location = new System.Drawing.Point(405, 211);
             this.winnersNamesLbl.Name = "winnersNamesLbl";
-            this.winnersNamesLbl.Size = new System.Drawing.Size(147, 25);
+            this.winnersNamesLbl.Size = new System.Drawing.Size(147, 18);
             this.winnersNamesLbl.TabIndex = 31;
             this.winnersNamesLbl.Text = "(hgkhjhg, uhg, label1)";
             this.winnersNamesLbl.Visible = false;
